f43568e5fc6ee9a64ab3bd3ff53fdf2ecbc7e7b8
Alejo Parra <jose.parra@unionsoluciones.net>
2017-04-27 09:30:43 -0500

    Rest
    Se agrega ejemplo y projecto rest.

---
+++ arquitetura-rest/src/main/java/co/com/arquitectura/rest/prueba1/ExempleAplication.java
@@ -0,0 +1,8 @@
+package co.com.arquitectura.rest.prueba1;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+@ApplicationPath("rest")
+public class ExempleAplication extends Application {
+
+}
