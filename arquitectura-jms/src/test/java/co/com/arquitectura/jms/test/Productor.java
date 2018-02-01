5ae37e97ec67e6029f22583aca91b6716abf1610
Alejandro Parra <jose.parra@ingeneo.com.co>
2018-02-01 08:43:52 -0500

    Enero25 Se realizan pruebas de funcionamiento de jms mq

--- arquitectura-jms/src/test/java/co/com/arquitectura/jms/test/Productor.java
+++ arquitectura-jms/src/test/java/co/com/arquitectura/jms/test/Productor.java
@@ -3,39 +3,53 @@ package co.com.arquitectura.jms.test;
 import java.util.Properties;
 
 import javax.jms.JMSContext;
+import javax.jms.JMSException;
 import javax.jms.Queue;
 import javax.jms.QueueConnection;
 import javax.jms.QueueConnectionFactory;
 import javax.jms.QueueReceiver;
 import javax.jms.QueueSession;
 import javax.jms.Session;
-import javax.jms.Topic;
-import javax.jms.TopicConnection;
-import javax.jms.TopicConnectionFactory;
 import javax.naming.Context;
 import javax.naming.InitialContext;
-
+import javax.jms.Connection;
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.junit.Test;
 
 public class Productor {
 	private String MESSAGE = "Hello, world";
-	private String CONNECTION_FACTORY = "jms/remoteCF";
-	private String QUEUE = "java:jboss/exported/jms/queue/ExampleQueue";
+	private String CONNECTION_FACTORY = "java:/jms/RemoteConnectionFactory";
+	private String QUEUE = "java:/myJmsTest/MyQueue";
 	private String TOPIC = "jms/Topic";
-	private String USUARIO = "invitado";
-	private String PASSWORD = "1nv1t4d0";
-	
+	private String USUARIO = "myJmsUser";
+	private String PASSWORD = "myJmsPassword";
+	private Integer port = 4447;
+	private String server = "localhost";
 	@Test
+	public void activeMq() {
+		ActiveMQConnectionFactory connectionF = new ActiveMQConnectionFactory("tcp://localhost:4447");
+		try {
+			Connection connection = connectionF.createConnection();
+		} catch (JMSException e) {
+			e.printStackTrace();
+		} 
+	}
+	
+//	@Test
 	public void prueba() throws Exception{
 		Context namingContext;
 		JMSContext context;
 		Queue queue;
 		try {
+			
 			Properties prop = new Properties();
-			prop.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
-			prop.put(Context.PROVIDER_URL, "http-remoting://localhost:5445");
-			prop.put("jboss.naming.client.ejb.context", true);
 			prop.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+			prop.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
+			//prop.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
+			prop.put(Context.PROVIDER_URL, "remote://"+server+":"+port);
+//			prop.put("jboss.naming.client.ejb.context", true);
+//			prop.put("jboss.naming.client.connect.options.org.xnio.Options.SSL_STARTTLS", false);
+//			prop.put("jboss.naming.client.remote.connectionprovider.create.options.org.xnio.Options.SSL_ENABLE", true);
 			prop.put(Context.SECURITY_PRINCIPAL, USUARIO);
 			prop.put(Context.SECURITY_CREDENTIALS, PASSWORD);
 			
