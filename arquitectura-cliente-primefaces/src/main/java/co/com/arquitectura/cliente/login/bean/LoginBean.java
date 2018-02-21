7796c793b6f048551653d24e653c2fa805aabd93
Alejandro Parra <jose.parra@ingeneo.com.co>
2018-02-21 16:58:29 -0500

    Enero25 ajuste recuperacion stach
    Change-Id: I4b73771821e99295a6a59cb11ea33b4069a5c2b5

--- arquitectura-cliente-primefaces/src/main/java/co/com/arquitectura/cliente/login/bean/LoginBean.java
+++ arquitectura-cliente-primefaces/src/main/java/co/com/arquitectura/cliente/login/bean/LoginBean.java
@@ -1,11 +1,16 @@
 package co.com.arquitectura.cliente.login.bean;
 
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+
+import co.com.arquitectura.ejb_local.login.interfaces.ILoginLocal;
 
 @ManagedBean(name="loginBean")
 @ViewScoped
 public class LoginBean {
+	@EJB
+	private ILoginLocal login;
 	private String valor;
 	public LoginBean(){
 		valor = "texto";
